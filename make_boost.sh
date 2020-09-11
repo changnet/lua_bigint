93c0086dc48db889e4135b2e51f8748b0361752b
changnet <940477049@qq.com>
2020-09-11 10:55:14 +0800

    add make_boost.sh

--- make_boost.sh
+++ make_boost.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# sh do NOT support array，do run this script with sh
+# sh do NOT support array，do NOT run this script with sh
 
 SRC=../boost_1_74_0
 DST="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -10,31 +10,37 @@ DST="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #    rm -R ./boost
 # fi
 
-FILES=(
-"boost/multiprecision/cpp_int.hpp"
+LIST_F=$DST/BOOST_FILES
 
-"boost/cstdint.hpp"
-"boost/multiprecision/number.hpp"
-"boost/multiprecision/detail/integer_ops.hpp"
-"boost/multiprecision/detail/rebind.hpp"
-"boost/core/empty_value.hpp"
-"boost/array.hpp"
-"boost/type_traits/is_integral.hpp"
-"boost/type_traits/is_floating_point.hpp"
-"boost/multiprecision/cpp_int/cpp_int_config.hpp"
-"boost/multiprecision/rational_adaptor.hpp"
-"boost/multiprecision/traits/is_byte_container.hpp"
-"boost/predef/other/endian.h"
-"boost/integer/static_min_max.hpp"
-"boost/type_traits/common_type.hpp"
-"boost/type_traits/make_signed.hpp"
-"boost/multiprecision/cpp_int/checked.hpp"
-"boost/multiprecision/detail/constexpr.hpp"
-"boost/multiprecision/cpp_int/value_pack.hpp"
-)
+# run make and catch error message to find out what boost header is missing
+# this is ugly and slow but it works
+function find_boost_header()
+{
+    msg=$(make 2>&1)
 
-cd $SRC
-for F in ${FILES[@]}; do
-    echo $F
-    cp --parent $F $DST
+    F=`echo $msg \
+    | grep -Eo 'fatal error: boost(.*): No such file or directory'\
+    | sed 's/fatal error: \(.*\): No such file or directory/\1/'`
+
+    if [[ ! -z  $F  ]]; then
+        echo $F
+        echo $F >> $LIST_F
+        cd $SRC
+        cp --parent $F $DST
+        cd - > /dev/null
+
+        return 1
+    fi
+
+    return 0
+}
+
+# loop a few times to find missing header file
+for i in {1..1024}; do
+    find_boost_header
+
+    n=$?
+    if [[ "$n" -ne "1" ]]; then
+        break
+    fi
 done
